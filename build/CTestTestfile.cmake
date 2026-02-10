f42a4640e335ba9d27e1f756b05905c2743fd6ff
Mikolaj Trebacz <lecorbio@Mac.ht.home>
2026-02-10 14:45:37 +0100

    add cancel function

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikolaj/Desktop/projects/matching-engine
-# Build directory: /home/mikolaj/Desktop/projects/matching-engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_matching "/home/mikolaj/Desktop/projects/matching-engine/build/test_matching")
-set_tests_properties(test_matching PROPERTIES  _BACKTRACE_TRIPLES "/home/mikolaj/Desktop/projects/matching-engine/CMakeLists.txt;22;add_test;/home/mikolaj/Desktop/projects/matching-engine/CMakeLists.txt;0;")
